6a3a614e2ce70252827a924c47003d717d442d28
m01i0ng <alley.ma@qq.com>
2020-04-28 09:36:28 +0800

    finish 1.0.3

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,88 +1,171 @@
-import {Component} from '@tarojs/taro'
+import {useEffect, useState} from '@tarojs/taro'
 import {View} from '@tarojs/components'
 import './index.scss'
-import {ClButton, ClCard, ClForm, ClFormItem, ClInput, ClMessage, ClText} from "mp-colorui";
+import {ClButton, ClCard, ClForm, ClFormItem, ClInput, ClMessage, ClText, ClTitleBar} from "mp-colorui";
 
-export default class Index extends Component {
+const Index = () => {
+  // 计算元数据
+  const [bg, setBg] = useState(0)
+  const [target, setTarget] = useState(0)
+  const [weight, setWeight] = useState(0)
+  const [tdd, setTdd] = useState(0)
+  const [result, setResult] = useState('0')
+  // 其他状态
+  const [msg, setMsg] = useState('')
+  const [mode, setMode] = useState('')
+  const [msgShow, setMsgShow] = useState(false)
 
-  state = {
-    bg: 0,
-    weight: 0,
-    target: 0,
-    result: 0,
-    msg: '',
-    show: false
-  }
-
-  config = {
-    navigationBarTitleText: '胰岛素剂量校正'
-  }
-
-  compute = () => {
-    const {bg, weight, target} = this.state
-    let msg
-
-    if (!weight) {
-      msg = '请输入 患者体重'
-      this.setState({msg, show: true})
-      return
-    } else if (!bg) {
-      msg = '请输入 现测血糖值'
-      this.setState({msg, show: true})
-      return
-    } else if (!target) {
-      msg = '请输入 目标血糖值'
-      this.setState({msg, show: true})
-      return
+  useEffect(() => {
+    return () => {
+      clear()
     }
+  }, [])
 
-    const result = ((bg - target) / (3000 / weight * 18)).toFixed(6);
+  const compute = () => {
+    let msg;
 
-    this.setState({
-      result
-    })
+    if (mode === 'unknown') {
+      if (!bg) {
+        msg = '请输入 现测血糖值'
+        setMsg(msg)
+        setMsgShow(true)
+        return
+      }
+      if (!target) {
+        msg = '请输入 目标血糖值'
+        setMsg(msg)
+        setMsgShow(true)
+        return;
+      }
+      if (!weight) {
+        msg = '请输入 患者体重';
+        setMsg(msg);
+        setMsgShow(true);
+        return;
+      }
+
+      const cf = 3000 / (weight * 18);
+      const result = ((bg - target) / cf).toFixed(1);
+
+      setResult(result);
+    } else if (mode === 'known') {
+      if (!bg) {
+        msg = '请输入 现测血糖值'
+        setMsg(msg)
+        setMsgShow(true)
+        return;
+      }
+      if (!target) {
+        msg = '请输入 目标血糖值'
+        setMsg(msg)
+        setMsgShow(true)
+        return;
+      }
+      if (!tdd) {
+        msg = '请输入 日总剂量'
+        setMsg(msg)
+        setMsgShow(true)
+        return;
+      }
+
+      const cf = 1700 / (tdd * 18);
+      const result = ((bg - target) / cf).toFixed(1)
+
+      setResult(result)
+    }
   }
 
-  render() {
-    return (
-      <View className='index'>
-        <ClMessage message={this.state.msg} show={this.state.show} type='warn'
-                   onClose={() => this.setState({show: false})}/>
-        <ClCard>
-          <ClText textColor='blue' size='large'>
-            计算方式：
-          </ClText>
-          <ClText>
-            校正系数 = 3000 / 患者的体重 x 18
-          </ClText>
-          <ClText>
-            校正胰岛素剂量 = (现测血糖值 - 目标血糖值) / 校正系数
-          </ClText>
-        </ClCard>
-        <ClCard>
-          <ClText>结果：</ClText>
-          <ClText size='xslarge'>{this.state.result}</ClText>
-        </ClCard>
-        <ClCard>
-          <ClForm>
-            <ClFormItem required prop='weight' autoFocus>
-              <ClInput title='患者体重：' placeholder='kg' type='digit' clear adjustPosition
-                       onChange={value => this.setState({weight: value})}/>
-            </ClFormItem>
-            <ClFormItem required prop='bg'>
-              <ClInput title='现测血糖值：' placeholder='mmol/L' type='digit' clear adjustPosition
-                       onChange={value => this.setState({bg: value})}/>
-            </ClFormItem>
-            <ClFormItem required prop='target'>
-              <ClInput title='目标血糖值：' placeholder='mmol/L' type='digit' clear adjustPosition
-                       onChange={value => this.setState({target: value})}/>
-            </ClFormItem>
-          </ClForm>
-        </ClCard>
-        <ClCard>
-          <ClButton shape='round' long onClick={this.compute}>计算</ClButton>
-        </ClCard>
-      </View>
-    )
+  const modeSwitch = mode => {
+    setMode(mode)
+    clear()
   }
+
+  const clear = () => {
+    setBg(0)
+    setTarget(0)
+    setWeight(0)
+    setTdd(0)
+    setResult('0')
+  }
+
+  return (
+    <View className='index'>
+      {
+        mode ? (
+          <View>
+            <ClMessage message={msg} show={msgShow} type='warn'
+                       onClose={() => setMsgShow(false)}/>
+            <ClCard>
+              <ClText>结果：</ClText>
+              <ClText align='center' size='xslarge' text={result}/>
+            </ClCard>
+            <ClCard>
+              <ClForm>
+                <ClTitleBar title='现测血糖值(mmol/L)' type='icon' icon='triangledownfill' iconColor='blue' bgColor='blue'
+                            textColor='white'
+                            borderColor='light-blue'/>
+                <ClFormItem required prop='bg'>
+                  <ClInput value={bg || ''} type='digit' clear adjustPosition
+                           autoFocus
+                           onChange={value => setBg(Number(value))}/>
+                </ClFormItem>
+                <View style={{height: '10px'}}/>
+                <ClTitleBar title='目标血糖值(mmol/L)' type='icon' icon='triangledownfill' iconColor='blue' bgColor='blue'
+                            textColor='white'
+                            borderColor='light-blue'/>
+                <ClFormItem required prop='bg'>
+                  <ClInput value={target || ''} type='digit' clear adjustPosition
+                           onChange={value => setTarget(Number(value))}/>
+                </ClFormItem>
+                <View style={{height: '10px'}}/>
+                {
+                  mode === 'unknown' ? (
+                    <View>
+                      <ClTitleBar title='患者体重(kg)' type='icon' icon='triangledownfill' iconColor='blue' bgColor='blue'
+                                  textColor='white' borderColor='light-blue'/>
+                      <ClFormItem required prop='weight' autoFocus>
+                        <ClInput value={weight || ''} type='digit' clear adjustPosition
+                                 onChange={value => setWeight(Number(value))}/>
+                      </ClFormItem>
+                    </View>
+                  ) : (
+                    <View>
+                      <ClTitleBar title='日总剂量(IU)' type='icon' icon='triangledownfill' iconColor='blue' bgColor='blue'
+                                  textColor='white' borderColor='light-blue'/>
+                      <ClFormItem required prop='tdd'>
+                        <ClInput value={tdd || ''} type="digit" clear adjustPosition
+                                 onChange={value => setTdd(Number(value))}/>
+                      </ClFormItem>
+                    </View>
+                  )
+                }
+              </ClForm>
+            </ClCard>
+            <ClCard>
+              <ClButton shadow shape='round' long onClick={compute}>计算</ClButton>
+              <View style={{height: '10px'}}/>
+              <ClButton shadow shape='round' bgColor='red' long onClick={clear}>清空</ClButton>
+              <View style={{height: '10px'}}/>
+              <ClButton shadow shape='round' long
+                        onClick={() => setMode('')}>返回(当前：{mode === 'unknown' ? '未知' : '已知'}总剂量)</ClButton>
+            </ClCard>
+          </View>
+        ) : (
+          <View className='mode-container'>
+            <ClCard>
+              <ClButton shadow shape="round" onClick={() => modeSwitch('unknown')} long>未知胰岛素总剂量</ClButton>
+              <View style={{height: '10px'}}/>
+              <ClButton shadow shape="round" long onClick={() => modeSwitch('known')}>已知胰岛素总剂量</ClButton>
+            </ClCard>
+          </View>
+        )
+      }
+    </View>
+  )
 }
+
+Index.config = {
+  navigationBarTitleText: '胰岛素剂量校正'
+}
+
+export default Index
